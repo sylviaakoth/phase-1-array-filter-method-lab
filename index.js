9d62a7d93a493d3de051ac6170804bff34c077d0
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-24 16:16:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function findMatching(array,string){
+    const results = array.filter(element => {
+      return element.toLowerCase() === string.toLowerCase()
+    })
+
+    return results;
+}
+
+
+function fuzzyMatch(array,string){
+    const results = array.filter(element => element.slice(0,3).includes(string))
+    return results 
+}
+
+function matchName(driver,string){
+    const results = driver.filter(element => element.name === string )
+    return results;
+
+}
